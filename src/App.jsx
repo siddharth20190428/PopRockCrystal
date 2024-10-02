bc02eb956641374a8e728058077da29917c48431
Siddharth Sahu <siddharth201820@gmail.com>
2024-10-02 20:08:21 +0530

    refactored app

--- src/App.jsx
+++ src/App.jsx
@@ -6,72 +6,22 @@ import Products from "./components/Products";
 
 function App() {
   return (
-    <div style={{ position: "relative" }}>
-      <div
-        style={{
-          height: "100vh",
-          position: "absolute",
-          inset: "-1rem 0 0 0 ",
-        }}
-      >
-        <img
-          src="./hero-bg.jpeg"
-          style={{
-            position: "absolute",
-            width: "100%",
-            height: "100%",
-            inset: 0,
-            zIndex: -10,
-          }}
-        />
-        <img
-          src="./hero-curve.png"
-          style={{
-            position: "absolute",
-            width: "100%",
-            height: "60%",
-            inset: "auto 0 0 0",
-            zIndex: -9,
-          }}
-        />
+    <div className="app">
+      <div className="hero-bg-cont">
+        <img src="./hero-bg.jpeg" className="hero-bg" />
+        <img src="./hero-curve.png" className="hero-curve" />
       </div>
-      <div style={{ margin: "1rem 2rem" }}>
+      <div className="content">
         <Navbar />
         <Hero />
         <Products />
         <Highlight />
       </div>
-      <div
-        style={{
-          height: "100vh",
-          position: "absolute",
-          bottom: 200,
-          left: 0,
-          width: "100%",
-        }}
-      >
-        <img
-          src="./highlight-bg.jpeg"
-          style={{
-            position: "absolute",
-            width: "100%",
-            height: "100%",
-            inset: 0,
-            zIndex: -10,
-            opacity: 0.5,
-          }}
-        />
-        <img
-          src="./highlight-curve.png"
-          style={{
-            position: "absolute",
-            width: "100%",
-            height: "100%",
-            zIndex: -9,
-          }}
-        />
+      <div className="highlight-bg">
+        <img src="./highlight-bg.jpeg" />
+        <img src="./highlight-curve.png" className="highlight-curve" />
       </div>
-      <div style={{ background: "#0A294DCC", height: 200 }}></div>
+      <div className="footer"></div>
     </div>
   );
 }
